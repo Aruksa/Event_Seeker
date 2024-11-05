ca1972e83be6f08c1293645071f3c5bdc4f912e8
Anushua Ahmed <69108570+Aruksa@users.noreply.github.com>
2024-11-05 22:49:36 +0600

    M1 FINAL

--- generatefaker/generateFakeEvents.js
+++ generatefaker/generateFakeEvents.js
@@ -1,19 +1,11 @@
 const axios = require('axios');
 const { faker } = require('@faker-js/faker');
-const countryList = require("react-select-country-list");
 
 const userTokens = [
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNzMwNzUwNjYxfQ.SavQU6AkPDldRTBqkKXxpfXs53KM0gk8JPsvo2-OwDA', // Token for user 1
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNzMwNzUwNjE2fQ.1QmVRy5FI4XzqS_n4gFuGc64c3RG_CH-L0maWq1dZzc', // Token for user 2
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaWF0IjoxNzMwNzUwNjk2fQ.kMdjMV_FvymLsACRr7JZ-D9Imy9dOxsXi16wixzq45Y', // Token for user 3
 ];
-
-// Function to generate a random country from country list
-const getRandomCountry = () => {
-  const countries = countryList().getData();
-  const randomCountry = faker.helpers.arrayElement(countries);
-  return randomCountry.label;
-};
 
 // Helper function to get a random future date
 const getRandomFutureDate = (startDate, maxDaysAhead = 60) => {
@@ -41,7 +33,7 @@ const createFakeEvent = () => {
     title: faker.company.name(),
     venue: faker.company.catchPhrase(),
     city: faker.location.city(),
-    country: getRandomCountry(),
+    country: faker.location.country(),
     description: faker.lorem.sentence(),
     mode: faker.helpers.arrayElement(['Online', 'Physical', 'Hybrid']),
     thumbnail: faker.image.url({ width: 256, height: 256 }),
@@ -68,7 +60,7 @@ const generateAndPostEvents = async () => {
   for (const token of userTokens) {
     console.log(`Generating events for user with token: ${token}`);
 
-    const events = Array.from({ length: 10}, createFakeEvent);
+    const events = Array.from({ length: 10 }, createFakeEvent);
     for (const event of events) {
       await postFakeEvent(event, token);
     }
